7aa8e869e87110fa1489dc3ebd6d8c51867873e6
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-06 19:34:10 +0000

    D3 Sets

---
+++ lib/opal/d3/sets.rb
@@ -0,0 +1,59 @@
+module D3
+  class Set
+    def initialize(array=nil)
+      if block_given?
+        @native = `window.d3`.JS.set(array, proc{|x| yield(x)})
+      elsif array
+        @native = `window.d3`.JS.set(array)
+      else
+        @native = `window.d3`.JS.set()
+      end
+    end
+
+    def has?(item)
+      @native.JS.has(item)
+    end
+
+    def add(item)
+      @native.JS.add(item)
+      self
+    end
+
+    def remove(item)
+      @native.JS.remove(item)
+      self
+    end
+
+    def clear
+      @native.JS.clear()
+      self
+    end
+
+    def values
+      @native.JS.values()
+    end
+
+    def each(&block)
+      @native.JS.each(block)
+      self
+    end
+
+    def empty?
+      @native.JS.empty()
+    end
+
+    def size
+      @native.JS.size
+    end
+
+    def inspect
+      "#<D3::Set: {#{ values.join(", ") }}>"
+    end
+  end
+
+  class << self
+    def set(array=nil, &block)
+      D3::Set.new(array, &block)
+    end
+  end
+end
---
+++ spec/sets_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe "d3-collections - sets" do
+  it "d3.set" do
+    expect(D3.set).to be_instance_of(D3::Set)
+    expect(D3.set.values).to eq([])
+    expect(D3.set([1,-2,3,1]).values).to eq(["1","-2","3"])
+    expect(D3.set([1,-2,3,-1], &:abs).values).to eq(["1","2","3"])
+  end
+
+  it "set.has?" do
+    expect(D3.set(["1"]).has?("2")).to eq(false)
+    # everything converted to strings
+    expect(D3.set(["1"]).has?("1")).to eq(true)
+    expect(D3.set([1]).has?(1)).to eq(true)
+    expect(D3.set([1]).has?("1")).to eq(true)
+    expect(D3.set(["1"]).has?(1)).to eq(true)
+  end
+
+  it "set.add" do
+    s = D3.set()
+    s.add(1).add(2).add(3).add(1)
+    expect(s.values).to eq(["1", "2", "3"])
+  end
+
+  it "set.clear" do
+    s = D3.set()
+    s.add(1).add(2).add(3).clear().add(1)
+    expect(s.values).to eq(["1"])
+  end
+
+  it "set.values" do
+    expect(D3.set().values).to eq([])
+    expect(D3.set([1]).values).to eq(["1"])
+    expect(D3.set([1,2,"1","2",3]).values).to eq(["1","2","3"])
+  end
+
+  it "set.each" do
+    called = []
+    D3.set([1,2,3]).each{|x|
+      called << x
+    }
+    expect(called).to eq(["1","2","3"])
+  end
+
+  it "set.empty?" do
+    a = D3.set()
+    expect(a).to be_empty
+    b = D3.set([1])
+    expect(b).to_not be_empty
+  end
+
+  it "set.size" do
+    a = D3.set()
+    expect(a.size).to eq(0)
+    b = D3.set([1,2,3])
+    expect(b.size).to eq(3)
+  end
+end
