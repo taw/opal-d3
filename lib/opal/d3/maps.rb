26990ecd2e297d1ac030616dd3b44fbbb3a0eaac
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-06 20:20:50 +0000

    D3 Maps

---
+++ lib/opal/d3/maps.rb
@@ -0,0 +1,76 @@
+module D3
+  class Map
+    def initialize(object=nil)
+      if block_given?
+        @native = `window.d3`.JS.map(object, proc{|x| yield(x)})
+      elsif object
+        @native = `window.d3`.JS.map(object)
+      else
+        @native = `window.d3`.JS.map()
+      end
+    end
+
+    def has?(key)
+      @native.JS.has(key)
+    end
+
+    def get(key)
+      result = @native.JS.get(key)
+      `result === undefined ? nil : result`
+    end
+    alias_method :[], :get
+
+    def set(key, value)
+      @native.JS.set(key, value)
+      self
+    end
+    alias_method :[]=, :set
+
+    def remove(key)
+      @native.JS.remove(key)
+      self
+    end
+
+    def clear
+      @native.JS.clear()
+      self
+    end
+
+    def keys
+      @native.JS.keys()
+    end
+
+    def values
+      @native.JS.values()
+    end
+
+    def entries
+      @native.JS.entries()
+    end
+
+    def each(&block)
+      @native.JS.each(block)
+      self
+    end
+
+    def empty?
+      @native.JS.empty()
+    end
+
+    def size
+      @native.JS.size
+    end
+
+    def inspect
+      "#<D3::Map: {#{ entries.map{|o|
+        "#{o.key} => #{o.value}"
+      }.join(", ")}>"
+    end
+  end
+
+  class << self
+    def map(object=nil, &block)
+      D3::Map.new(object, &block)
+    end
+  end
+end
---
+++ spec/maps_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+
+describe "d3-collections - maps" do
+  it "d3.map" do
+    expect(D3.map).to be_instance_of(D3::Map)
+  end
+
+  it "d3.has?" do
+    expect(D3.map().has?("1")).to eq(false)
+    expect(D3.map(["a","b"]).has?("0")).to eq(true)
+    expect(D3.map(["a","b"]).has?(0)).to eq(true)
+    expect(D3.map(["a","b"]).has?(2)).to eq(false)
+    expect(D3.map(["a","b"]).has?("a")).to eq(false)
+  end
+
+  it "d3.get" do
+    a = D3.map(["a", "b"])
+    expect(nil).to eq(nil)
+    expect(a[0]).to eq("a")
+    expect(a[2]).to eq(nil)
+    expect(a.get(0)).to eq("a")
+    expect(a.get(2)).to eq(nil)
+  end
+
+  it "d3.set" do
+    a = D3.map.set("foo", 1).set("bar", 2)
+    a["baz"] = 3
+    expect(a["foo"]).to eq(1)
+    expect(a["bar"]).to eq(2)
+    expect(a["baz"]).to eq(3)
+  end
+
+  it "map.remove" do
+    a = D3.map.set("a", 1).set("b", 2).remove("a").set("c", 3)
+    expect(a.keys).to eq(["b", "c"])
+  end
+
+  it "map.clear" do
+    a = D3.map.set("a", 1).set("b", 2).clear.set("c", 3)
+    expect(a.keys).to eq(["c"])
+  end
+
+  it "map.keys" do
+    a = D3.map.set("foo", 1).set("bar", 2)
+    expect(a.keys).to eq(["foo", "bar"])
+  end
+
+  it "map.values" do
+    a = D3.map.set("foo", 1).set("bar", 2)
+    expect(a.values).to eq([1, 2])
+  end
+
+  it "map.entries" do
+    a = D3.map.set("foo", 1).set("bar", 2)
+    expect(a.entries.map{|x| `JSON.stringify(x)`}).to eq([
+      '{"key":"foo","value":1}',
+      '{"key":"bar","value":2}',
+    ])
+  end
+
+  it "map.each" do
+    called = []
+    a = D3.map.set("foo", 1).set("bar", 2)
+    a.each do |v,k|
+      called << [k,v]
+    end
+    expect(called).to eq([["foo",1], ["bar",2]])
+
+  end
+
+  it "map.empty" do
+    expect(D3.map).to be_empty
+    expect(D3.map(["a"])).to_not be_empty
+  end
+
+  it "map.size" do
+    expect(D3.map.size).to eq(0)
+    expect(D3.map(["a"]).size).to eq(1)
+  end
+end
