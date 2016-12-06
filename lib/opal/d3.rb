62f2a8e7f19b5cd57a942657d21f5efd7e05ae6c
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-06 17:20:26 +0000

    DRY up

--- lib/opal/d3.rb
+++ lib/opal/d3.rb
@@ -1,6 +1,20 @@
 require "opal"
+require "opal-parser"
+
+class Module
+  def alias_d3(ruby_name, js_name=ruby_name)
+    eval <<-EOF
+      def #{ruby_name}(*args)
+        @d3.JS.#{js_name}(*args)
+      end
+    EOF
+  end
+end
 
 module D3
+  @d3 = `window.d3`
+  class << self
+  end
 end
 
 require_relative "d3/statistics"
--- lib/opal/d3/transformations.rb
+++ lib/opal/d3/transformations.rb
@@ -1,39 +1,13 @@
 module D3
   class << self
-    def merge(arrays)
-      `window.d3`.JS.merge(arrays)
-    end
-
-    def pairs(array)
-      `window.d3`.JS.pairs(array)
-    end
-
-    def permute(array, indexes)
-      `window.d3`.JS.permute(array, indexes)
-    end
-
-    def shuffle(*args)
-      `window.d3`.JS.shuffle(*args)
-    end
-
-    def ticks(start, stop, count)
-      `window.d3`.JS.ticks(start, stop, count)
-    end
-
-    def tick_step(start, stop, count)
-      `window.d3`.JS.tickStep(start, stop, count)
-    end
-
-    def range(*args)
-      `window.d3`.JS.range(*args)
-    end
-
-    def transpose(array)
-      `window.d3`.JS.transpose(array)
-    end
-
-    def zip(*arrays)
-      `window.d3`.JS.zip(*arrays)
-    end
+    alias_d3 :merge
+    alias_d3 :pairs
+    alias_d3 :permute
+    alias_d3 :shuffle
+    alias_d3 :ticks
+    alias_d3 :tick_step, :tickStep
+    alias_d3 :range
+    alias_d3 :transpose
+    alias_d3 :zip
   end
 end
