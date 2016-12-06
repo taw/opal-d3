2024ad5d84fd392b53669e9bd4e4559c138e1b93
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-06 15:39:51 +0000

    Histogram class

--- lib/opal/d3/histograms.rb
+++ lib/opal/d3/histograms.rb
@@ -1,5 +1,36 @@
 module D3
+  class Histogram
+    def initialize
+      @native = `window.d3.histogram()`
+    end
+
+    def call(data)
+      @native.call(data)
+    end
+
+    def thresholds(count=nil, &block)
+      if block_given?
+        @native.JS.thresholds(block)
+      elsif count.is_a?(Numeric)
+        @native.JS.thresholds(count)
+      elsif count == :scott
+        @native.JS.thresholds{|*args| D3.threshold_scott(*args)}
+      elsif count == :sturges
+        @native.JS.thresholds{|*args| D3.threshold_sturges(*args)}
+      elsif count == :freedman_diaconis
+        @native.JS.thresholds{|*args| D3.threshold_freedman_diaconis(*args)}
+      else
+        raise ArgumentError, "Wrong use of D3::Histogram.thresholds API - pass block, number, or symbol"
+      end
+      self
+    end
+  end
+
   class << self
+    def histogram
+      D3::Histogram.new
+    end
+
     def threshold_freedman_diaconis(*args)
       `window.d3`.JS.thresholdFreedmanDiaconis(*args)
     end
--- spec/histograms_spec.rb
+++ spec/histograms_spec.rb
@@ -1,6 +1,51 @@
 require 'spec_helper'
 
 describe "d3-array - histograms" do
+  it "#histogram" do
+    expect(D3.histogram).to be_a(D3::Histogram)
+  end
+
+  describe "histogram.thresholds" do
+    let(:data) { (1..10).to_a }
+    let(:h) { D3.histogram }
+    it "block" do
+      h.thresholds{|*args| D3.threshold_sturges(*args) }
+      expect(h.(data)).to eq([
+        [1],
+        [2,3],
+        [4,5],
+        [6,7],
+        [8,9,10]
+      ])
+    end
+
+    it "symbol" do
+      h.thresholds(:scott)
+      expect(h.(data)).to eq([
+        [1,2,3,4],
+        [5,6,7,8,9,10],
+      ])
+    end
+
+    it "number" do
+      h.thresholds(2)
+      expect(h.(data)).to eq([
+        [1, 2, 3, 4],
+        [5, 6, 7, 8, 9, 10]
+      ])
+    end
+  end
+
+  it "histogram.call" do
+    h = D3.histogram
+    expect(h.([1,2,1,2,1,3])).to eq([
+      [1,1,1],
+      [],
+      [2,2],
+      [3],
+    ])
+  end
+
   it "#threshold_freedman_diaconis" do
     data = (0..10).to_a
     expect(D3.threshold_freedman_diaconis(data,0,10)).to eq(3)
