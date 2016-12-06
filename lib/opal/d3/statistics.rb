ae7f0e87da9cd7527d8f63f40c8de942e477eaf9
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-06 17:23:57 +0000

    DRY up a bit more

--- lib/opal/d3/statistics.rb
+++ lib/opal/d3/statistics.rb
@@ -2,78 +2,78 @@ module D3
   class << self
     def min(data, &block)
       if block
-        `window.d3`.JS.min(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
+        @d3.JS.min(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
       else
-        `window.d3`.JS.min(data.compact)
+        @d3.JS.min(data.compact)
       end
     end
 
     def max(data, &block)
       if block
-        `window.d3`.JS.max(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
+        @d3.JS.max(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
       else
-        `window.d3`.JS.max(data.compact)
+        @d3.JS.max(data.compact)
       end
     end
 
     def sum(data, &block)
       if block
-        `window.d3`.JS.sum(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
+        @d3.JS.sum(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
       else
-        `window.d3`.JS.sum(data.compact)
+        @d3.JS.sum(data.compact)
       end
     end
 
     def mean(data, &block)
       result = if block
-        `window.d3`.JS.mean(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
+        @d3.JS.mean(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
       else
-        `window.d3`.JS.mean(data.compact)
+        @d3.JS.mean(data.compact)
       end
       `result === undefined ? nil : result`
     end
 
     def median(data, &block)
       result = if block
-        `window.d3`.JS.median(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
+        @d3.JS.median(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
       else
-        `window.d3`.JS.median(data.compact)
+        @d3.JS.median(data.compact)
       end
       `result === undefined ? nil : result`
     end
 
     def deviation(data, &block)
       result = if block
-        `window.d3`.JS.deviation(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
+        @d3.JS.deviation(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
       else
-        `window.d3`.JS.deviation(data.compact)
+        @d3.JS.deviation(data.compact)
       end
       `result === undefined ? nil : result`
     end
 
     def variance(data, &block)
       result = if block
-        `window.d3`.JS.variance(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
+        @d3.JS.variance(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
       else
-        `window.d3`.JS.variance(data.compact)
+        @d3.JS.variance(data.compact)
       end
       `result === undefined ? nil : result`
     end
 
     def quantile(data, p, &block)
       result = if block
-        `window.d3`.JS.quantile(data, p, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
+        @d3.JS.quantile(data, p, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
       else
-        `window.d3`.JS.quantile(data.compact, p)
+        @d3.JS.quantile(data.compact, p)
       end
       `result === undefined ? nil : result`
     end
 
     def extent(data, &block)
       (a,b) = if block
-        `window.d3`.JS.extent(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
+        @d3.JS.extent(data, proc{|x| y=yield(x); y.nil? ? `undefined` : y})
       else
-        `window.d3`.JS.extent(data.compact)
+        @d3.JS.extent(data.compact)
       end
       [`a === undefined ? nil : a`, `b === undefined ? nil : b`]
     end
