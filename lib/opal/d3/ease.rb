9fc98e5eccb2df16efb1e6a656e8f888a0e864f0
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-07 00:06:44 +0000

    Straightforward ease types

---
+++ lib/opal/d3/ease.rb
@@ -0,0 +1,29 @@
+module D3
+  class <<self
+    alias_d3 :ease_linear, :easeLinear
+    alias_d3 :ease_quad, :easeQuad
+    alias_d3 :ease_quad_in, :easeQuadIn
+    alias_d3 :ease_quad_out, :easeQuadOut
+    alias_d3 :ease_quad_in_out, :easeQuadInOut
+    alias_d3 :ease_cubic, :easeCubic
+    alias_d3 :ease_cubic_in, :easeCubicIn
+    alias_d3 :ease_cubic_out, :easeCubicOut
+    alias_d3 :ease_cubic_in_out, :easeCubicInOut
+    alias_d3 :ease_sin, :easeSin
+    alias_d3 :ease_sin_in, :easeSinIn
+    alias_d3 :ease_sin_out, :easeSinOut
+    alias_d3 :ease_sin_in_out, :easeSinInOut
+    alias_d3 :ease_exp, :easeExp
+    alias_d3 :ease_exp_in, :easeExpIn
+    alias_d3 :ease_exp_out, :easeExpOut
+    alias_d3 :ease_exp_in_out, :easeExpInOut
+    alias_d3 :ease_circle, :easeCircle
+    alias_d3 :ease_circle_in, :easeCircleIn
+    alias_d3 :ease_circle_out, :easeCircleOut
+    alias_d3 :ease_circle_in_out, :easeCircleInOut
+    alias_d3 :ease_bounce, :easeBounce
+    alias_d3 :ease_bounce_in, :easeBounceIn
+    alias_d3 :ease_bounce_out, :easeBounceOut
+    alias_d3 :ease_bounce_in_out, :easeBounceInOut
+  end
+end
