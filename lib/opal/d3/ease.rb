ff119d139b53dbb410e2cfcf4ec67d946ea2209b
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-11 18:04:42 +0000

    d3 ease currying variants

--- lib/opal/d3/ease.rb
+++ lib/opal/d3/ease.rb
@@ -1,29 +1,203 @@
 module D3
   class <<self
-    alias_d3 :ease_linear, :easeLinear
-    alias_d3 :ease_quad, :easeQuad
-    alias_d3 :ease_quad_in, :easeQuadIn
-    alias_d3 :ease_quad_out, :easeQuadOut
-    alias_d3 :ease_quad_in_out, :easeQuadInOut
-    alias_d3 :ease_cubic, :easeCubic
-    alias_d3 :ease_cubic_in, :easeCubicIn
-    alias_d3 :ease_cubic_out, :easeCubicOut
-    alias_d3 :ease_cubic_in_out, :easeCubicInOut
-    alias_d3 :ease_sin, :easeSin
-    alias_d3 :ease_sin_in, :easeSinIn
-    alias_d3 :ease_sin_out, :easeSinOut
-    alias_d3 :ease_sin_in_out, :easeSinInOut
-    alias_d3 :ease_exp, :easeExp
-    alias_d3 :ease_exp_in, :easeExpIn
-    alias_d3 :ease_exp_out, :easeExpOut
-    alias_d3 :ease_exp_in_out, :easeExpInOut
-    alias_d3 :ease_circle, :easeCircle
-    alias_d3 :ease_circle_in, :easeCircleIn
-    alias_d3 :ease_circle_out, :easeCircleOut
-    alias_d3 :ease_circle_in_out, :easeCircleInOut
-    alias_d3 :ease_bounce, :easeBounce
-    alias_d3 :ease_bounce_in, :easeBounceIn
-    alias_d3 :ease_bounce_out, :easeBounceOut
-    alias_d3 :ease_bounce_in_out, :easeBounceInOut
+    def ease_linear(t=nil)
+      if t
+        @d3.JS.easeLinear(t)
+      else
+        `#@d3.easeLinear`
+      end
+    end
+
+    def ease_quad(t=nil)
+      if t
+        @d3.JS.easeQuad(t)
+      else
+        `#@d3.easeQuad`
+      end
+    end
+
+    def ease_quad_in(t=nil)
+      if t
+        @d3.JS.easeQuadIn(t)
+      else
+        `#@d3.easeQuadIn`
+      end
+    end
+
+    def ease_quad_out(t=nil)
+      if t
+        @d3.JS.easeQuadOut(t)
+      else
+        `#@d3.easeQuadOut`
+      end
+    end
+
+    def ease_quad_in_out(t=nil)
+      if t
+        @d3.JS.easeQuadInOut(t)
+      else
+        `#@d3.easeQuadInOut`
+      end
+    end
+
+    def ease_cubic(t=nil)
+      if t
+        @d3.JS.easeCubic(t)
+      else
+        `#@d3.easeCubic`
+      end
+    end
+
+    def ease_cubic_in(t=nil)
+      if t
+        @d3.JS.easeCubicIn(t)
+      else
+        `#@d3.easeCubicIn`
+      end
+    end
+
+    def ease_cubic_out(t=nil)
+      if t
+        @d3.JS.easeCubicOut(t)
+      else
+        `#@d3.easeCubicOut`
+      end
+    end
+
+    def ease_cubic_in_out(t=nil)
+      if t
+        @d3.JS.easeCubicInOut(t)
+      else
+        `#@d3.easeCubicInOut`
+      end
+    end
+
+    def ease_sin(t=nil)
+      if t
+        @d3.JS.easeSin(t)
+      else
+        `#@d3.easeSin`
+      end
+    end
+
+    def ease_sin_in(t=nil)
+      if t
+        @d3.JS.easeSinIn(t)
+      else
+        `#@d3.easeSinIn`
+      end
+    end
+
+    def ease_sin_out(t=nil)
+      if t
+        @d3.JS.easeSinOut(t)
+      else
+        `#@d3.easeSinOut`
+      end
+    end
+
+    def ease_sin_in_out(t=nil)
+      if t
+        @d3.JS.easeSinInOut(t)
+      else
+        `#@d3.easeSinInOut`
+      end
+    end
+
+    def ease_exp(t=nil)
+      if t
+        @d3.JS.easeExp(t)
+      else
+        `#@d3.easeExp`
+      end
+    end
+
+    def ease_exp_in(t=nil)
+      if t
+        @d3.JS.easeExpIn(t)
+      else
+        `#@d3.easeExpIn`
+      end
+    end
+
+    def ease_exp_out(t=nil)
+      if t
+        @d3.JS.easeExpOut(t)
+      else
+        `#@d3.easeExpOut`
+      end
+    end
+
+    def ease_exp_in_out(t=nil)
+      if t
+        @d3.JS.easeExpInOut(t)
+      else
+        `#@d3.easeExpInOut`
+      end
+    end
+
+    def ease_circle(t=nil)
+      if t
+        @d3.JS.easeCircle(t)
+      else
+        `#@d3.easeCircle`
+      end
+    end
+
+    def ease_circle_in(t=nil)
+      if t
+        @d3.JS.easeCircleIn(t)
+      else
+        `#@d3.easeCircleIn`
+      end
+    end
+
+    def ease_circle_out(t=nil)
+      if t
+        @d3.JS.easeCircleOut(t)
+      else
+        `#@d3.easeCircleOut`
+      end
+    end
+
+    def ease_circle_in_out(t=nil)
+      if t
+        @d3.JS.easeCircleInOut(t)
+      else
+        `#@d3.easeCircleInOut`
+      end
+    end
+
+    def ease_bounce(t=nil)
+      if t
+        @d3.JS.easeBounce(t)
+      else
+        `#@d3.easeBounce`
+      end
+    end
+
+    def ease_bounce_in(t=nil)
+      if t
+        @d3.JS.easeBounceIn(t)
+      else
+        `#@d3.easeBounceIn`
+      end
+    end
+
+    def ease_bounce_out(t=nil)
+      if t
+        @d3.JS.easeBounceOut(t)
+      else
+        `#@d3.easeBounceOut`
+      end
+    end
+
+    def ease_bounce_in_out(t=nil)
+      if t
+        @d3.JS.easeBounceInOut(t)
+      else
+        `#@d3.easeBounceInOut`
+      end
+    end
   end
 end
--- spec/ease_spec.rb
+++ spec/ease_spec.rb
@@ -1,9 +1,11 @@
 describe "d3-ease" do
   let(:range) { (0..20).map{|x| x / 20 }}
   let(:curve) { range.map{|t| D3.send(ease, t).round(4) }}
+  let(:curve_curry) { range.map{|t| D3.send(ease).call(t).round(4) } }
   describe "d3.ease_linear" do
     let(:ease) { :ease_linear }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1])
     end
   end
@@ -11,6 +13,7 @@ describe "d3-ease" do
   describe "d3.ease_quad" do
     let(:ease) { :ease_quad }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.005, 0.02, 0.045, 0.08, 0.125, 0.18, 0.245, 0.32, 0.405, 0.5, 0.595, 0.68, 0.755, 0.82, 0.875, 0.92, 0.955, 0.98, 0.995, 1])
     end
   end
@@ -18,6 +21,7 @@ describe "d3-ease" do
   describe "d3.ease_quad_in" do
     let(:ease) { :ease_quad_in }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0025, 0.01, 0.0225, 0.04, 0.0625, 0.09, 0.1225, 0.16, 0.2025, 0.25, 0.3025, 0.36, 0.4225, 0.49, 0.5625, 0.64, 0.7225, 0.81, 0.9025, 1])
     end
   end
@@ -25,6 +29,7 @@ describe "d3-ease" do
   describe "d3.ease_quad_out" do
     let(:ease) { :ease_quad_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0975, 0.19, 0.2775, 0.36, 0.4375, 0.51, 0.5775, 0.64, 0.6975, 0.75, 0.7975, 0.84, 0.8775, 0.91, 0.9375, 0.96, 0.9775, 0.99, 0.9975, 1])
     end
   end
@@ -32,6 +37,7 @@ describe "d3-ease" do
   describe "d3.ease_quad_in_out" do
     let(:ease) { :ease_quad_in_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.005, 0.02, 0.045, 0.08, 0.125, 0.18, 0.245, 0.32, 0.405, 0.5, 0.595, 0.68, 0.755, 0.82, 0.875, 0.92, 0.955, 0.98, 0.995, 1])
     end
   end
@@ -39,6 +45,7 @@ describe "d3-ease" do
   describe "d3.ease_cubic" do
     let(:ease) { :ease_cubic }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0005, 0.004, 0.0135, 0.032, 0.0625, 0.108, 0.1715, 0.256, 0.3645, 0.5, 0.6355, 0.744, 0.8285, 0.892, 0.9375, 0.968, 0.9865, 0.996, 0.9995, 1])
     end
   end
@@ -46,6 +53,7 @@ describe "d3-ease" do
   describe "d3.ease_cubic_in" do
     let(:ease) { :ease_cubic_in }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0001, 0.001, 0.0034, 0.008, 0.0156, 0.027, 0.0429, 0.064, 0.0911, 0.125, 0.1664, 0.216, 0.2746, 0.343, 0.4219, 0.512, 0.6141, 0.729, 0.8574, 1])
     end
   end
@@ -53,6 +61,7 @@ describe "d3-ease" do
   describe "d3.ease_cubic_out" do
     let(:ease) { :ease_cubic_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.1426, 0.271, 0.3859, 0.488, 0.5781, 0.657, 0.7254, 0.784, 0.8336, 0.875, 0.9089, 0.936, 0.9571, 0.973, 0.9844, 0.992, 0.9966, 0.999, 0.9999, 1])
     end
   end
@@ -60,6 +69,7 @@ describe "d3-ease" do
   describe "d3.ease_cubic_in_out" do
     let(:ease) { :ease_cubic_in_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0005, 0.004, 0.0135, 0.032, 0.0625, 0.108, 0.1715, 0.256, 0.3645, 0.5, 0.6355, 0.744, 0.8285, 0.892, 0.9375, 0.968, 0.9865, 0.996, 0.9995, 1])
     end
   end
@@ -67,6 +77,7 @@ describe "d3-ease" do
   describe "d3.ease_sin" do
     let(:ease) { :ease_sin }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0062, 0.0245, 0.0545, 0.0955, 0.1464, 0.2061, 0.273, 0.3455, 0.4218, 0.5, 0.5782, 0.6545, 0.727, 0.7939, 0.8536, 0.9045, 0.9455, 0.9755, 0.9938, 1])
     end
   end
@@ -74,6 +85,7 @@ describe "d3-ease" do
   describe "d3.ease_sin_in" do
     let(:ease) { :ease_sin_in }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0031, 0.0123, 0.0276, 0.0489, 0.0761, 0.109, 0.1474, 0.191, 0.2396, 0.2929, 0.3506, 0.4122, 0.4775, 0.546, 0.6173, 0.691, 0.7666, 0.8436, 0.9215, 1])
     end
   end
@@ -81,6 +93,7 @@ describe "d3-ease" do
   describe "d3.ease_sin_out" do
     let(:ease) { :ease_sin_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0785, 0.1564, 0.2334, 0.309, 0.3827, 0.454, 0.5225, 0.5878, 0.6494, 0.7071, 0.7604, 0.809, 0.8526, 0.891, 0.9239, 0.9511, 0.9724, 0.9877, 0.9969, 1])
     end
   end
@@ -88,6 +101,7 @@ describe "d3-ease" do
   describe "d3.ease_sin_in_out" do
     let(:ease) { :ease_sin_in_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0062, 0.0245, 0.0545, 0.0955, 0.1464, 0.2061, 0.273, 0.3455, 0.4218, 0.5, 0.5782, 0.6545, 0.727, 0.7939, 0.8536, 0.9045, 0.9455, 0.9755, 0.9938, 1])
     end
   end
@@ -95,6 +109,7 @@ describe "d3-ease" do
   describe "d3.ease_exp" do
     let(:ease) { :ease_exp }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0.0005, 0.001, 0.002, 0.0039, 0.0078, 0.0156, 0.0313, 0.0625, 0.125, 0.25, 0.5, 0.75, 0.875, 0.9375, 0.9688, 0.9844, 0.9922, 0.9961, 0.998, 0.999, 0.9995])
     end
   end
@@ -102,6 +117,7 @@ describe "d3-ease" do
   describe "d3.ease_exp_in" do
     let(:ease) { :ease_exp_in }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0.001, 0.0014, 0.002, 0.0028, 0.0039, 0.0055, 0.0078, 0.011, 0.0156, 0.0221, 0.0313, 0.0442, 0.0625, 0.0884, 0.125, 0.1768, 0.25, 0.3536, 0.5, 0.7071, 1])
     end
   end
@@ -109,6 +125,7 @@ describe "d3-ease" do
   describe "d3.ease_exp_out" do
     let(:ease) { :ease_exp_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.2929, 0.5, 0.6464, 0.75, 0.8232, 0.875, 0.9116, 0.9375, 0.9558, 0.9688, 0.9779, 0.9844, 0.989, 0.9922, 0.9945, 0.9961, 0.9972, 0.998, 0.9986, 0.999])
     end
   end
@@ -116,6 +133,7 @@ describe "d3-ease" do
   describe "d3.ease_exp_in_out" do
     let(:ease) { :ease_exp_in_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0.0005, 0.001, 0.002, 0.0039, 0.0078, 0.0156, 0.0313, 0.0625, 0.125, 0.25, 0.5, 0.75, 0.875, 0.9375, 0.9688, 0.9844, 0.9922, 0.9961, 0.998, 0.999, 0.9995])
     end
   end
@@ -123,6 +141,7 @@ describe "d3-ease" do
   describe "d3.ease_circle" do
     let(:ease) { :ease_circle }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0025, 0.0101, 0.023, 0.0417, 0.067, 0.1, 0.1429, 0.2, 0.2821, 0.5, 0.7179, 0.8, 0.8571, 0.9, 0.933, 0.9583, 0.977, 0.9899, 0.9975, 1])
     end
   end
@@ -130,6 +149,7 @@ describe "d3-ease" do
   describe "d3.ease_circle_in" do
     let(:ease) { :ease_circle_in }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0013, 0.005, 0.0113, 0.0202, 0.0318, 0.0461, 0.0633, 0.0835, 0.107, 0.134, 0.1648, 0.2, 0.2401, 0.2859, 0.3386, 0.4, 0.4732, 0.5641, 0.6878, 1])
     end
   end
@@ -137,6 +157,7 @@ describe "d3-ease" do
   describe "d3.ease_circle_out" do
     let(:ease) { :ease_circle_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.3122, 0.4359, 0.5268, 0.6, 0.6614, 0.7141, 0.7599, 0.8, 0.8352, 0.866, 0.893, 0.9165, 0.9367, 0.9539, 0.9682, 0.9798, 0.9887, 0.995, 0.9987, 1])
     end
   end
@@ -144,6 +165,7 @@ describe "d3-ease" do
   describe "d3.ease_circle_in_out" do
     let(:ease) { :ease_circle_in_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0025, 0.0101, 0.023, 0.0417, 0.067, 0.1, 0.1429, 0.2, 0.2821, 0.5, 0.7179, 0.8, 0.8571, 0.9, 0.933, 0.9583, 0.977, 0.9899, 0.9975, 1])
     end
   end
@@ -151,6 +173,7 @@ describe "d3-ease" do
   describe "d3.ease_bounce" do
     let(:ease) { :ease_bounce }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0189, 0.0756, 0.1702, 0.3025, 0.4727, 0.6806, 0.9264, 0.91, 0.8189, 0.7656, 0.7502, 0.7725, 0.8327, 0.9306, 0.9727, 0.94, 0.9452, 0.9881, 0.9845, 1])
     end
   end
@@ -158,6 +181,7 @@ describe "d3-ease" do
   describe "d3.ease_bounce_in" do
     let(:ease) { :ease_bounce_in }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0155, 0.0119, 0.0548, 0.06, 0.0273, 0.0694, 0.1673, 0.2275, 0.2498, 0.2344, 0.1811, 0.09, 0.0736, 0.3194, 0.5273, 0.6975, 0.8298, 0.9244, 0.9811, 1])
     end
   end
@@ -165,6 +189,7 @@ describe "d3-ease" do
   describe "d3.ease_bounce_out" do
     let(:ease) { :ease_bounce_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0189, 0.0756, 0.1702, 0.3025, 0.4727, 0.6806, 0.9264, 0.91, 0.8189, 0.7656, 0.7502, 0.7725, 0.8327, 0.9306, 0.9727, 0.94, 0.9452, 0.9881, 0.9845, 1])
     end
   end
@@ -172,6 +197,7 @@ describe "d3-ease" do
   describe "d3.ease_bounce_in_out" do
     let(:ease) { :ease_bounce_in_out }
     it do
+      expect(curve).to eq(curve_curry)
       expect(curve).to eq([0, 0.0059, 0.03, 0.0347, 0.1138, 0.1172, 0.045, 0.1597, 0.3488, 0.4622, 0.5, 0.5378, 0.6512, 0.8403, 0.955, 0.8828, 0.8863, 0.9653, 0.97, 0.9941, 1])
     end
   end
