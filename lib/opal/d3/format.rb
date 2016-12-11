950197a248a93c6af74e14b04dbb1238a72f4626
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-11 04:22:20 +0000

    Finished d3 number formatters

--- lib/opal/d3/format.rb
+++ lib/opal/d3/format.rb
@@ -29,11 +29,34 @@ module D3
     end
   end
 
+  class FormatLocale
+    def initialize(native)
+      raise unless native
+      @native = native
+    end
+
+    def format(specifier)
+      if specifier.is_a?(FormatSpecifier)
+        @native.JS.format(specifier.to_n)
+      else
+        @native.JS.format(specifier)
+      end
+    end
+
+    def format_prefix(specifier, value)
+      if specifier.is_a?(FormatSpecifier)
+        @native.JS.formatPrefix(specifier.to_n, value)
+      else
+        @native.JS.formatPrefix(specifier, value)
+      end
+    end
+  end
+
+
   class << self
     alias_d3 :precision_fixed, :precisionFixed
     alias_d3 :precision_prefix, :precisionPrefix
     alias_d3 :precision_round, :precisionRound
-    alias_d3 :format_prefix, :formatPrefix
 
     def format(specifier)
       if specifier.is_a?(FormatSpecifier)
@@ -43,8 +66,38 @@ module D3
       end
     end
 
+    def format_prefix(specifier, value)
+      if specifier.is_a?(FormatSpecifier)
+        @d3.JS.formatPrefix(specifier.to_n, value)
+      else
+        @d3.JS.formatPrefix(specifier, value)
+      end
+    end
+
     def format_specifier(specifier)
       D3::FormatSpecifier.new @d3.JS.formatSpecifier(specifier)
+    end
+
+    def format_locale(spec={})
+      D3::FormatLocale.new @d3.JS.formatLocale(
+        {
+          decimal: spec.fetch("decimal", "."),
+          thousands: spec.fetch("thousands", ","),
+          grouping: spec.fetch("grouping", [3]),
+          currency: spec.fetch("currency", ["$", ""]),
+        }.to_n
+      )
+    end
+
+    def format_default_locale(spec={})
+      D3::FormatLocale.new @d3.JS.formatDefaultLocale(
+        {
+          decimal: spec.fetch("decimal", "."),
+          thousands: spec.fetch("thousands", ","),
+          grouping: spec.fetch("grouping", [3]),
+          currency: spec.fetch("currency", ["$", ""]),
+        }.to_n
+      )
     end
   end
 end
--- spec/format_spec.rb
+++ spec/format_spec.rb
@@ -4,6 +4,13 @@ describe "d3 - format" do
     expect(D3.format(".2").(4.2)).to eq("4.2")
     expect(D3.format(".1").(42)).to eq("4e+1")
     expect(D3.format(".1").(4.2)).to eq("4")
+    expect(D3.format(".0%").(0.123)).to eq("12%")
+    expect(D3.format("($.2f").(-3.5)).to eq("($3.50)")
+    expect(D3.format("+20").(42)).to eq("                 +42")
+    expect(D3.format(".^20").(42)).to eq(".........42.........")
+    expect(D3.format(".2s").(42e6)).to eq("42M")
+    expect(D3.format("#x").(48879)).to eq("0xbeef")
+    expect(D3.format(",.2r").(4223)).to eq("4,200")
   end
 
   it "d3.format_prefix" do
@@ -41,5 +48,40 @@ describe "d3 - format" do
 
   it "d3.precision_round" do
     expect(D3.precision_round(0.01, 1.01)).to eq(3)
+  end
+
+  let(:pl) {{
+    decimal: ",",
+    thousands: ".",
+    grouping: [3],
+    currency: ["", "zł"],
+  }}
+  let(:us) {{
+    decimal: ".",
+    thousands: ",",
+    grouping: [3],
+    currency: ["$", ""],
+  }}
+  it "d3.format_locale" do
+    locale = D3.format_locale(pl)
+    expect(locale).to be_instance_of(D3::FormatLocale)
+    expect(locale.format("$.2f").(-3.5)).to eq("-3,50zł")
+    expect(locale.format("$.2f").(123456.789)).to eq("123456,79zł")
+    expect(locale.format(",.2r").(4223)).to eq("4.200")
+    expect(locale.format_prefix(",.0", 1e-3).(123456.789012)).to eq("123.456,789m")
+  end
+
+  # This test affects global state, so we must be sure to restore it
+  it "d3.format_default_locale" do
+    expect(D3.format(",.2r").(4223)).to eq("4,200")
+
+    locale_pl = D3.format_default_locale(pl)
+    expect(D3.format(",.2r").(4223)).to eq("4.200")
+    expect(locale_pl.format(",.2r").(4223)).to eq("4.200")
+
+    locale_us = D3.format_default_locale(us)
+    expect(D3.format(",.2r").(4223)).to eq("4,200")
+    expect(locale_pl.format(",.2r").(4223)).to eq("4.200")
+    expect(locale_us.format(",.2r").(4223)).to eq("4,200")
   end
 end
