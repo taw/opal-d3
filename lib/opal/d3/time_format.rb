6acf4e2aae4760e62f702dae9491cd80ba7d7155
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-12 03:27:52 +0000

    D3 time format

---
+++ lib/opal/d3/time_format.rb
@@ -0,0 +1,53 @@
+module D3
+  class TimeFormatLocale
+    def initialize(native)
+      raise unless native
+      @native = native
+    end
+
+    include Native
+    alias_native :format, :format
+    alias_native :parse, :parse
+    alias_native :utc_format, :utcFormat
+    alias_native :utc_parse, :utcParse
+  end
+
+  class << self
+    alias_d3 :time_format, :timeFormat
+    alias_d3 :time_parse, :timeParse
+    alias_d3 :utc_format, :utcFormat
+    alias_d3 :utc_parse, :utcParse
+    alias_d3 :iso_format, :isoFormat
+    alias_d3 :iso_parse, :isoParse
+
+    def time_format_locale(spec={})
+      D3::TimeFormatLocale.new @d3.JS.timeFormatLocale(
+        {
+          dateTime: spec.fetch(:date_time, "%x, %X"),
+          date: spec.fetch(:date, "%-m/%-d/%Y"),
+          time: spec.fetch(:time, "%-I:%M:%S %p"),
+          periods: spec.fetch(:periods, ["AM", "PM"]),
+          days: spec.fetch(:days, ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]),
+          shortDays: spec.fetch(:short_days, ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]),
+          months: spec.fetch(:months, ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]),
+          shortMonths: spec.fetch(:short_months, ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]),
+        }.to_n
+      )
+    end
+
+    def time_format_default_locale(spec={})
+      D3::TimeFormatLocale.new @d3.JS.timeFormatDefaultLocale(
+        {
+          dateTime: spec.fetch(:date_time, "%x, %X"),
+          date: spec.fetch(:date, "%-m/%-d/%Y"),
+          time: spec.fetch(:time, "%-I:%M:%S %p"),
+          periods: spec.fetch(:periods, ["AM", "PM"]),
+          days: spec.fetch(:days, ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]),
+          shortDays: spec.fetch(:short_days, ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]),
+          months: spec.fetch(:months, ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]),
+          shortMonths: spec.fetch(:short_months, ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]),
+        }.to_n
+      )
+    end
+  end
+end
---
+++ spec/time_format_spec.rb
@@ -0,0 +1,99 @@
+require "time"
+
+# Running these specs in weird timezone will fail because javascript timezones are pile of fail
+describe "d3 - time format" do
+  let(:us) {{
+    date_time: "%x, %X",
+    date: "%-m/%-d/%Y",
+    time: "%-I:%M:%S %p",
+    periods: ["AM", "PM"],
+    days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    short_days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    short_months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+  }}
+  let(:pl) {{
+    date_time: "%A, %e %B %Y, %X",
+    date: "%d/%m/%Y",
+    time: "%H:%M:%S",
+    periods: ["AM", "PM"],
+    days: ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"],
+    short_days: ["Niedz.", "Pon.", "Wt.", "Śr.", "Czw.", "Pt.", "Sob."],
+    months: ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"],
+    short_months: ["Stycz.", "Luty", "Marz.", "Kwie.", "Maj", "Czerw.", "Lipc.", "Sierp.", "Wrz.", "Paźdz.", "Listop.", "Grudz."],
+  }}
+
+  it "d3.time_format" do
+    f = D3.time_format("%B %d, %Y")
+    expect(f).to be_instance_of(Proc)
+    expect(f.(Time.parse("June 30, 2015"))).to eq("June 30, 2015")
+  end
+
+  it "d3.time_parse" do
+    f = D3.time_parse("%B %d, %Y")
+    expect(f).to be_instance_of(Proc)
+    expect(f.("June 30, 2015")).to eq(Time.parse("June 30, 2015 00:00:00"))
+  end
+
+  it "d3.utc_format" do
+    f = D3.utc_format("%B %d, %Y")
+    expect(f).to be_instance_of(Proc)
+    expect(f.(Time.parse("June 30, 2015 12:00:00"))).to eq("June 30, 2015")
+  end
+
+  it "d3.utc_parse" do
+    f = D3.utc_parse("%B %d, %Y")
+    expect(f).to be_instance_of(Proc)
+    expect(f.("June 30, 2015")).to eq(Time.parse("June 30, 2015 01:00:00"))
+  end
+
+  it "d3.iso_format / d3.iso_parse" do
+    t = Time.parse("June 30, 2015 12:30:45")
+    s = "2015-06-30T11:30:45.000Z"
+    expect(D3.iso_format(t)).to eq(s)
+    expect(D3.iso_parse(s)).to eq(t)
+  end
+
+  describe "locale" do
+    let(:locale) { D3.time_format_locale(pl) }
+
+    it "d3.format" do
+      f = locale.format("%B %d, %Y")
+      expect(f).to be_instance_of(Proc)
+      expect(f.(Time.parse("June 30, 2015"))).to eq("Czerwiec 30, 2015")
+    end
+
+    it "d3.parse" do
+      f = locale.parse("%B %d, %Y")
+      expect(f).to be_instance_of(Proc)
+      expect(f.("Czerwiec 30, 2015")).to eq(Time.parse("June 30, 2015 00:00:00"))
+    end
+
+    it "d3.utc_format" do
+      f = locale.utc_format("%B %d, %Y")
+      expect(f).to be_instance_of(Proc)
+      expect(f.(Time.parse("June 30, 2015 12:00:00"))).to eq("Czerwiec 30, 2015")
+    end
+
+    it "d3.utc_parse" do
+      f = locale.utc_parse("%B %d, %Y")
+      expect(f).to be_instance_of(Proc)
+      expect(f.("Czerwiec 30, 2015")).to eq(Time.parse("June 30, 2015 01:00:00"))
+    end
+  end
+
+  # This test affects global state, so we must be sure to restore it
+  it "d3.time_format_default_locale" do
+    t = Time.parse("June 30, 2015 12:30:00")
+    expect(D3.time_format("%c").(t)).to eq("6/30/2015, 12:30:00 PM")
+
+    locale_pl = D3.time_format_default_locale(pl)
+    expect(D3.time_format("%c").(t)).to eq("Wtorek, 30 Czerwiec 2015, 12:30:00")
+
+    locale_us = D3.time_format_default_locale(us)
+    expect(D3.time_format("%c").(t)).to eq("6/30/2015, 12:30:00 PM")
+
+    expect(locale_pl.format("%c").(t)).to eq("Wtorek, 30 Czerwiec 2015, 12:30:00")
+    expect(locale_us.format("%c").(t)).to eq("6/30/2015, 12:30:00 PM")
+  end
+end
