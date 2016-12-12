d16fbc65cc5372fb6f633dec1b65934df7aa8c39
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-12 16:45:28 +0000

    Predefined ordinal scales

---
+++ lib/opal/d3/ordinal_scale.rb
@@ -0,0 +1,19 @@
+module D3
+  class << self
+    def scheme_category_10
+      `window.d3.schemeCategory10`
+    end
+
+    def scheme_category_20
+      `window.d3.schemeCategory20`
+    end
+
+    def scheme_category_20b
+      `window.d3.schemeCategory20b`
+    end
+
+    def scheme_category_20c
+      `window.d3.schemeCategory20c`
+    end
+  end
+end
