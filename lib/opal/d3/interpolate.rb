e2a52df0aec035ca0762ab9cd6aadda20d922c51
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-13 22:24:25 +0000

    Extend d3-interpolate interface to support magic currying (like d3-ease)

--- lib/opal/d3/interpolate.rb
+++ lib/opal/d3/interpolate.rb
@@ -1,21 +1,125 @@
 module D3
   class << self
-    alias_d3 :interpolate_number, :interpolateNumber
-    alias_d3 :interpolate_round, :interpolateRound
-    alias_d3 :interpolate_string, :interpolateString
-    alias_d3 :interpolate_array, :interpolateArray
-    alias_d3 :interpolate_date, :interpolateDate
+    def interpolate_number(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateNumber`
+      else
+        @d3.JS.interpolateNumber(a,b)
+      end
+    end
+
+    def interpolate_round(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateRound`
+      else
+        @d3.JS.interpolateRound(a,b)
+      end
+    end
+
+    def interpolate_string(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateString`
+      else
+        @d3.JS.interpolateString(a,b)
+      end
+    end
+
+    def interpolate_array(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateArray`
+      else
+        @d3.JS.interpolateArray(a,b)
+      end
+    end
+
+    def interpolate_date(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateDate`
+      else
+        @d3.JS.interpolateDate(a,b)
+      end
+    end
 
     # These should support gamma
-    alias_d3 :interpolate_rgb, :interpolateRgb
-    alias_d3 :interpolate_rgb_basis, :interpolateRgbBasis
-    alias_d3 :interpolate_rgb_basis_closed, :interpolateRgbBasisClosed
-    alias_d3 :interpolate_hsl, :interpolateHsl
-    alias_d3 :interpolate_hsl_long, :interpolateHslLong
-    alias_d3 :interpolate_lab, :interpolateLab
-    alias_d3 :interpolate_hcl, :interpolateHcl
-    alias_d3 :interpolate_hcl_long, :interpolateHclLong
-    alias_d3 :interpolate_cubehelix, :interpolateCubehelix
-    alias_d3 :interpolate_cubehelix_long, :interpolateCubehelixLong
+    def interpolate_rgb(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateRgb`
+      else
+        @d3.JS.interpolateRgb(a,b)
+      end
+    end
+
+    def interpolate_rgb_basis(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateRgbBasis`
+      else
+        @d3.JS.interpolateRgbBasis(a,b)
+      end
+    end
+
+    def interpolate_rgb_basis_closed(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateRgbBasisClosed`
+      else
+        @d3.JS.interpolateRgbBasisClosed(a,b)
+      end
+    end
+
+    def interpolate_hsl(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateHsl`
+      else
+        @d3.JS.interpolateHsl(a,b)
+      end
+    end
+
+    def interpolate_hsl_long(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateHslLong`
+      else
+        @d3.JS.interpolateHslLong(a,b)
+      end
+    end
+
+    def interpolate_lab(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateLab`
+      else
+        @d3.JS.interpolateLab(a,b)
+      end
+    end
+
+    def interpolate_hcl(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateHcl`
+      else
+        @d3.JS.interpolateHcl(a,b)
+      end
+    end
+
+    def interpolate_hcl_long(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateHclLong`
+      else
+        @d3.JS.interpolateHclLong(a,b)
+      end
+    end
+
+    def interpolate_cubehelix(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateCubehelix`
+      else
+        @d3.JS.interpolateCubehelix(a,b)
+      end
+    end
+
+    def interpolate_cubehelix_long(a=nil,b=nil)
+      if a == nil and b == nil
+        `#@d3.interpolateCubehelixLong`
+      else
+        @d3.JS.interpolateCubehelixLong(a,b)
+      end
+    end
+
   end
 end
--- spec/interpolate_spec.rb
+++ spec/interpolate_spec.rb
@@ -1,78 +1,152 @@
 describe "d3 - interpolate" do
-  it "d3.interpolate_number" do
-    f = D3.interpolate_number(10,20)
-    expect(f.(0.3)).to eq(13)
-    expect(f.(0.31)).to eq(13.1)
+  let(:f) { D3.send(name, a, b) }
+  let(:v) { f.(0.5) }
+  let(:f2) { D3.send(name).(a, b) }
+  let(:v2) { f2.(0.5) }
+
+  describe "numbers" do
+    let(:a) { 10 }
+    let(:b) { 20 }
+
+    describe "d3.interpolate_number" do
+      let(:name) { :interpolate_number }
+      it do
+        expect(f.(0.3)).to eq(13)
+        expect(f.(0.31)).to eq(13.1)
+        expect(f.(0.31)).to eq(f2.(0.31))
+      end
+    end
+
+    describe "d3.interpolate_round" do
+      let(:name) { :interpolate_round }
+      it do
+        expect(f.(0.3)).to eq(13)
+        expect(f.(0.31)).to eq(13)
+        expect(f.(0.31)).to eq(f2.(0.31))
+      end
+    end
   end
 
-  it "d3.interpolate_round" do
-    f = D3.interpolate_round(10,20)
-    expect(f.(0.3)).to eq(13)
-    expect(f.(0.31)).to eq(13)
+  describe "d3.interpolate_string" do
+    let(:a) { "300 12px sans-serif" }
+    let(:b) { "500 36px Comic-Sans" }
+    let(:name) { :interpolate_string }
+
+    it do
+      expect(f.(0.5)).to eq("400 24px Comic-Sans")
+      expect(f.(0.5)).to eq(f2.(0.5))
+    end
   end
 
-  it "d3.interpolate_string" do
-    f = D3.interpolate_string("300 12px sans-serif", "500 36px Comic-Sans")
-    expect(f.(0.5)).to eq("400 24px Comic-Sans")
+  describe "d3.interpolate_array" do
+    let(:a) { [0, 200] }
+    let(:b) { [10, 500] }
+    let(:name) { :interpolate_array }
+
+    it do
+      expect(f.(0.5)).to eq([5, 350])
+      expect(f.(0.5)).to eq(f2.(0.5))
+    end
   end
 
-  it "d3.interpolate_array" do
-    f = D3.interpolate_array([0, 200], [10, 500])
-    expect(f.(0.5)).to eq([5, 350])
+  describe "d3.interpolate_date" do
+    let(:a) { Time.parse("June 30, 2015 00:00:00 UTC") }
+    let(:b) { Time.parse("December 31, 2016 12:00:00 UTC") }
+    let(:name) { :interpolate_date }
+
+    it do
+      expect(f.(0.5)).to eq(Time.parse("March 31, 2016 06:00:00 UTC"))
+      expect(f.(0.5)).to eq(f2.(0.5))
+    end
   end
 
-  it "d3.interpolate_date" do
-    f = D3.interpolate_date(Time.parse("June 30, 2015 00:00:00 UTC"), Time.parse("December 31, 2016 12:00:00 UTC"))
-    expect(f.(0.5)).to eq(Time.parse("March 31, 2016 06:00:00 UTC"))
-  end
+  describe "colors" do
+    let(:a) { "purple" }
+    let(:b) { "orange" }
 
-  it "d3.interpolate_rgb" do
-    f = D3.interpolate_rgb("purple", "orange")
-    expect(f.(0.5)).to eq("rgb(192, 83, 64)")
-  end
+    describe "d3.interpolate_rgb" do
+      let(:name) { :interpolate_rgb }
+      it do
+        expect(v).to eq("rgb(192, 83, 64)")
+        expect(v).to eq(v2)
+      end
+    end
 
-  it "d3.interpolate_rgb_basis" do
-    f = D3.interpolate_rgb_basis(["purple", "orange"])
-    expect(f.(0.5)).to eq("rgb(192, 83, 64)")
-  end
+    describe "basis" do
+      let(:f) { D3.send(name, [a, b]) }
+      let(:f2) { D3.send(name).([a, b]) }
 
-  it "d3.interpolate_rgb_basis_closed" do
-    f = D3.interpolate_rgb_basis_closed(["purple", "orange"])
-    expect(f.(0.5)).to eq("rgb(213, 110, 43)")
-  end
+      describe "d3.interpolate_rgb_basis" do
+        let(:name) { :interpolate_rgb_basis }
+        it do
+          expect(v).to eq("rgb(192, 83, 64)")
+          expect(v).to eq(v2)
+        end
+      end
 
-  it "d3.interpolate_hsl" do
-    f = D3.interpolate_hsl("purple", "orange")
-    expect(f.(0.5)).to eq("rgb(192, 0, 34)")
-  end
+      describe "d3.interpolate_rgb_basis_closed" do
+        let(:name) { :interpolate_rgb_basis_closed }
+        it do
+          expect(v).to eq("rgb(213, 110, 43)")
+          expect(v).to eq(v2)
+        end
+      end
+    end
 
-  it "d3.interpolate_hsl_long" do
-    f = D3.interpolate_hsl_long("purple", "orange")
-    expect(f.(0.5)).to eq("rgb(0, 192, 158)")
-  end
+    describe "d3.interpolate_hsl" do
+      let(:name) { :interpolate_hsl }
+      it do
+        expect(v).to eq("rgb(192, 0, 34)")
+        expect(v).to eq(v2)
+      end
+    end
 
-  it "d3.interpolate_lab" do
-    f = D3.interpolate_lab("purple", "orange")
-    expect(f.(0.5)).to eq("rgb(197, 93, 91)")
-  end
+    describe "d3.interpolate_hsl_long" do
+      let(:name) { :interpolate_hsl_long }
+      it do
+        expect(v).to eq("rgb(0, 192, 158)")
+        expect(v).to eq(v2)
+      end
+    end
 
-  it "d3.interpolate_hcl" do
-    f = D3.interpolate_hcl("purple", "orange")
-    expect(f.(0.5)).to eq("rgb(236, 46, 83)")
-  end
+    describe "d3.interpolate_lab" do
+      let(:name) { :interpolate_lab }
+      it do
+        expect(v).to eq("rgb(197, 93, 91)")
+        expect(v).to eq(v2)
+      end
+    end
 
-  it "d3.interpolate_hcl_long" do
-    f = D3.interpolate_hcl_long("purple", "orange")
-    expect(f.(0.5)).to eq("rgb(0, 153, 169)")
-  end
+    describe "d3.interpolate_hcl" do
+      let(:name) { :interpolate_hcl }
+      it do
+        expect(v).to eq("rgb(236, 46, 83)")
+        expect(v).to eq(v2)
+      end
+    end
 
-  it "d3.interpolate_cubehelix" do
-    f = D3.interpolate_cubehelix("purple", "orange")
-    expect(f.(0.5)).to eq("rgb(255, 32, 68)")
-  end
+    describe "d3.interpolate_hcl_long" do
+      let(:name) { :interpolate_hcl_long }
+      it do
+        expect(v).to eq("rgb(0, 153, 169)")
+        expect(v).to eq(v2)
+      end
+    end
 
-  it "d3.interpolate_cubehelix_long" do
-    f = D3.interpolate_cubehelix_long("purple", "orange")
-    expect(f.(0.5)).to eq("rgb(0, 194, 158)")
+    describe "d3.interpolate_cubehelix" do
+      let(:name) { :interpolate_cubehelix }
+      it do
+        expect(v).to eq("rgb(255, 32, 68)")
+        expect(v).to eq(v2)
+      end
+    end
+
+    describe "d3.interpolate_cubehelix_long" do
+      let(:name) { :interpolate_cubehelix_long }
+      it do
+        expect(v).to eq("rgb(0, 194, 158)")
+        expect(v).to eq(v2)
+      end
+    end
   end
 end
