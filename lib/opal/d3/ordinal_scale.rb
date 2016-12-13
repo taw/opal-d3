be946d41e28295df83984b14da25b1aff9eb2c18
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-13 21:00:03 +0000

    Ordinal scales

--- lib/opal/d3/ordinal_scale.rb
+++ lib/opal/d3/ordinal_scale.rb
@@ -1,5 +1,45 @@
 module D3
+  class OrdinalScale
+    def initialize(native)
+      raise unless native
+      @native = native
+    end
+
+    def call(t)
+      @native.call(t)
+    end
+    attribute_d3 :domain
+    attribute_d3 :range
+    alias_native_new :copy
+
+    # D3 is trying to reinvent Ruby symbols here
+    def unknown(new_value=nil)
+      if new_value == nil
+        v = @native.JS.unknown
+        if `JSON.stringify(v) === '{"name":"implicit"}'`
+          :implicit
+        else
+          v
+        end
+      else
+        if new_value == :implicit
+          new_value = `window.d3.scaleImplicit`
+        end
+        @native.JS.unknown(new_value)
+        self
+      end
+    end
+  end
+
   class << self
+    def scale_ordinal(*args)
+      D3::OrdinalScale.new @d3.JS.scaleOrdinal(*args)
+    end
+
+    def scale_implicit
+      :implicit
+    end
+
     def scheme_category_10
       `window.d3.schemeCategory10`
     end
--- spec/ordinal_scale_spec.rb
+++ spec/ordinal_scale_spec.rb
@@ -1,4 +1,46 @@
 describe "d3 ordinal scale" do
+  it "d3.scale_ordinal" do
+    expect(D3.scale_ordinal).to be_instance_of(D3::OrdinalScale)
+  end
+
+  let(:d) { ("a".."z").to_a }
+  let(:r) { D3.scheme_category_10 }
+  let(:colors) { D3.scale_ordinal.domain(d).range(r) }
+
+  it "basics" do
+    expect(colors.domain).to eq(d)
+    expect(colors.range).to eq(r)
+    expect(colors.("c")).to eq("#2ca02c")
+    expect(colors.("z")).to eq("#8c564b")
+    # default implicit
+    expect(colors.("X")).to eq("#e377c2")
+    expect(colors.("Y")).to eq("#7f7f7f")
+    expect(colors.("X")).to eq("#e377c2")
+  end
+
+  it "unknown" do
+    expect(D3.scale_implicit).to eq(:implicit)
+    expect(colors.unknown).to eq(:implicit)
+
+    colors.unknown("#ffffff")
+    expect(colors.unknown).to eq("#ffffff")
+    expect(colors.("c")).to eq("#2ca02c")
+    expect(colors.("C")).to eq("#ffffff")
+
+    colors.unknown(:implicit)
+    expect(colors.unknown).to eq(:implicit)
+    expect(colors.("c")).to eq("#2ca02c")
+    expect(colors.("C")).to eq("#e377c2")
+  end
+
+  it "copy" do
+    cc = colors.copy.domain([1,2,3])
+    expect(colors.domain).to eq(d)
+    expect(cc.domain).to eq([1,2,3])
+    expect(colors.range).to eq(r)
+    expect(cc.range).to eq(r)
+  end
+
   it "d3.scheme_category_10" do
     expect(D3.scheme_category_10).to eq(["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"])
   end
