c2e0eac9908b66e1f56d5b3a26b7f5b3a44c1086
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-12-30 08:34:21 +0000

    Cleaned up elections visualization

--- demo/app/data/elections_2016.rb
+++ demo/app/data/elections_2016.rb
@@ -1,8 +1,10 @@
+require "ostruct"
+
 Elections2016 = [
-  { name: "Donald Trump", votes: 62686675, color: "#ff8c00" },
-  { name: "Hillary Clinton", votes: 65240114, color: "#98abc5" },
-  { name: "Gary Johnson", votes: 4460666, color: "#8a89a6" },
-  { name: "Jill Stein", votes: 1440193, color: "#7b6888" },
-  { name: "Evan McMullin", votes: 642386, color: "#6b486b" },
-  { name: "Darrell Castle", votes: 198576, color: "#a05d56" },
-]
+  { name: "Donald Trump", votes: 62_979_879, color: "#ff8c00" },
+  { name: "Hillary Clinton", votes: 65_844_954, color: "#98abc5" },
+  { name: "Gary Johnson", votes: 4_488_919, color: "#8a89a6" },
+  { name: "Jill Stein", votes: 1_457_044, color: "#7b6888" },
+  { name: "Evan McMullin", votes: 725_902, color: "#6b486b" },
+  { name: "Darrell Castle", votes: 202_979, color: "#a05d56" },
+].map{|o| OpenStruct.new(o) }
--- demo/app/elections_2016.rb
+++ demo/app/elections_2016.rb
@@ -3,9 +3,17 @@ require "opal-d3"
 require "data/elections_2016"
 
 list = D3.select("#visualization").append("ul")
+max_votes = Elections2016.map(&:votes).max
 
 Elections2016.each do |candidate|
   item = list.append("li")
-  item.text = "#{candidate[:name]} - #{candidate[:votes]} votes"
-  item.style("background-color", candidate[:color])
+    .style("position", "relative")
+  item.append("span")
+    .style("background-color", candidate.color)
+    .style("position", "absolute")
+    .style("width", "#{100.0 * candidate.votes / max_votes}%")
+    .style("height", "100%")
+  item.append("span")
+    .style("position", "absolute")
+    .text("#{candidate.name} - #{candidate.votes} votes")
 end
