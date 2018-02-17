6d5db8e0498e5f1cd7407bed1906c241501646eb
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-02-17 20:09:40 +0000

    Improved visualization: Olympics 2016 medals

--- demo/app/olympics_2016_medals.rb
+++ demo/app/olympics_2016_medals.rb
@@ -1,4 +1,3 @@
-require "opal"
 require "opal-d3"
 require "data/olympics_2016_medals"
 
@@ -14,41 +13,54 @@ max_medals = Olympics2016Medals.map(&:total).max
 x = D3.scale_linear.domain([0, max_medals]).range([0, width-50])
 y = D3.scale_linear.domain([0, Olympics2016Medals.size]).range([0, height])
 
-svg.append("g").attr("transform", "translate(50, 0)")
-  .select_all("rect")
-  .data(Olympics2016Medals)
-  .enter.append("rect")
-    .attr("height", 20)
-    .attr("width"){|d| x.(d.gold)}
-    .attr("x", 0)
-    .attr("y"){|d,i| y.(i)}
-    .attr("fill", "gold")
+chart_area = svg.append("g").attr("transform", "translate(50, 0)")
 
-svg.append("g").attr("transform", "translate(50, 0)")
-  .select_all("rect")
+medal = chart_area.append("g")
+  .select_all("g")
   .data(Olympics2016Medals)
-  .enter.append("rect")
-    .attr("height", 20)
-    .attr("width"){|d| x.(d.silver)}
-    .attr("x"){|d| x.(d.gold) }
-    .attr("y"){|d,i| y.(i)}
-    .attr("fill", "silver")
+  .enter.append("g")
 
-svg.append("g").attr("transform", "translate(50, 0)")
-  .select_all("rect")
-  .data(Olympics2016Medals)
-  .enter.append("rect")
-    .attr("height", 20)
-    .attr("width"){|d| x.(d.bronze)}
-    .attr("x"){|d| x.(d.gold + d.silver) }
-    .attr("y"){|d,i| y.(i)}
-    .attr("fill", "#CD7F32")
+medal.append("rect")
+  .attr("height", 20)
+  .attr("width"){|d| x.(d.gold) }
+  .attr("x", 0)
+  .attr("y"){|d,i| y.(i) }
+  .attr("fill", "gold")
 
-svg.append("g")
-  .select_all("text")
-  .data(Olympics2016Medals)
-  .enter.append("text")
-    .attr("x", 0)
-    .attr("y"){|d,i| y.(i) + 16}
-    .text{|d| d.acronym}
-    .style("font-size", "20px")
+medal.append("text")
+  .text{|d| d.gold == 0 ? "" : d.gold }
+  .attr("x"){|d| x.(d.gold/2) }
+  .attr("y"){|d,i| y.(i+0.7) }
+  .style("text-anchor", "middle")
+
+medal.append("rect")
+  .attr("height", 20)
+  .attr("width"){|d| x.(d.silver)}
+  .attr("x"){|d| x.(d.gold) }
+  .attr("y"){|d,i| y.(i) }
+  .attr("fill", "silver")
+
+medal.append("text")
+  .text{|d| d.silver == 0 ? "" : d.silver }
+  .attr("x"){|d| x.(d.gold + d.silver/2) }
+  .attr("y"){|d,i| y.(i+0.7) }
+  .style("text-anchor", "middle")
+
+medal.append("rect")
+  .attr("height", 20)
+  .attr("width"){|d| x.(d.bronze) }
+  .attr("x"){|d| x.(d.gold + d.silver) }
+  .attr("y"){|d,i| y.(i) }
+  .attr("fill", "#CD7F32")
+
+medal.append("text")
+  .text{|d| d.bronze == 0 ? "" : d.bronze}
+  .attr("x"){|d| x.(d.gold + d.silver + d.bronze/2) }
+  .attr("y"){|d,i| y.(i+0.7) }
+  .style("text-anchor", "middle")
+
+medal.append("text")
+  .attr("x", -50)
+  .attr("y"){|d,i| y.(i+0.7) }
+  .text{|d| d.acronym }
+  .style("font-size", "20px")
