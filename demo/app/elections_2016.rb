ac0b140349727497b9784b57585d5177c76ad898
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-02-17 19:37:40 +0000

    Improved visualization: Elections 2016

--- demo/app/elections_2016.rb
+++ demo/app/elections_2016.rb
@@ -1,4 +1,3 @@
-require "opal"
 require "opal-d3"
 require "data/elections_2016"
 
@@ -6,14 +5,16 @@ list = D3.select("#visualization").append("ul")
 max_votes = Elections2016.map(&:votes).max
 
 Elections2016.each do |candidate|
+  vote_count_formatted = D3.format("3,d").(candidate.votes)
   item = list.append("li")
     .style("position", "relative")
   item.append("span")
     .style("background-color", candidate.color)
+    .style("opacity", 0.5)
     .style("position", "absolute")
     .style("width", "#{100.0 * candidate.votes / max_votes}%")
     .style("height", "100%")
   item.append("span")
     .style("position", "absolute")
-    .text("#{candidate.name} - #{candidate.votes} votes")
+    .text("#{candidate.name} - #{vote_count_formatted} votes")
 end
