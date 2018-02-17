0081d8e7d00099a44f6c06b8e8839c578478da49
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-02-17 20:57:37 +0000

    Improved visualization: iPhones

--- demo/app/iphones.rb
+++ demo/app/iphones.rb
@@ -1,11 +1,10 @@
-require "opal"
 require "opal-d3"
 require "data/iphones"
 
 svg = D3.select("#visualization")
-        .append("svg")
-        .attr("height", "500px")
-        .attr("width", "100%")
+  .append("svg")
+  .attr("height", "500px")
+  .attr("width", "100%")
 width = svg.style("width").to_i
 
 x = D3.scale_linear.domain(IPhoneVariants.map(&:released).minmax).range([20,width-90]).nice
@@ -17,7 +16,7 @@ c = D3.scale_ordinal.range(D3.scheme_category_20)
 duplicates = Hash.new(0)
 
 graph_area = svg.append("g")
-    .attr("transform", "translate(60, 20)")
+  .attr("transform", "translate(60, 20)")
 graph_area.select_all("circle")
   .data(IPhoneVariants).enter
   .append("circle")
@@ -26,22 +25,30 @@ graph_area.select_all("circle")
       duplicates[[d.released, d.size]] += 1
       x.(d.released) + 15 * count
     }
-    .attr("cy"){|d| y.(d.size)}
+    .attr("cy"){|d| y.(d.size) }
     .attr("r", 10)
-    .attr("fill"){|d| c.(d.name)}
+    .attr("fill"){|d| c.(d.name) }
+    .append("title")
+      .text{|d| "#{d.name} - #{d.size}GB" }
+
+axis_left = D3.axis_left(y)
+  .tick_values([4,8,16,32,64,128,256])
+  .tick_format{|v| "#{v} GB" }
+graph_area.call(axis_left)
 
 axis_bottom = D3.axis_bottom(x)
   .tick_format(D3.time_format("%Y-%m-%d"))
-axis_left = D3.axis_left(y)
-  .tick_values([4,8,16,32,64,128,256])
-  .tick_format{|v| "#{v} GB"}
-graph_area.call(axis_left)
-graph_area.append("g").attr("transform", "translate(0, 400)").call(axis_bottom)
+graph_area.append("g")
+  .attr("transform", "translate(0, 400)")
+  .call(axis_bottom)
 
-D3.select("#visualization").append("table")
-  .select_all("tr")
+D3.select("#visualization")
+  .append("div")
+  .style("display", "flex")
+  .select_all("div")
   .data(IPhones).enter
-  .append("tr")
-    .append("td")
-    .text{|d| d.name}
-    .style("background"){|d| c.(d.name)}
+    .append("div")
+    .text{|d| d.name }
+    .style("background"){|d| c.(d.name) }
+    .style("padding", "0.5em")
+    .style("margin", "0.5em")
