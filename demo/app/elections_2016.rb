b04eb145adb0add126a7af3d465570a5a7ab7c8b
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-02-18 19:24:08 +0000

    Switched to a system with separate js and css

--- demo/app/elections_2016.rb
+++ demo/app/elections_2016.rb
@@ -1,20 +1,17 @@
 require "opal-d3"
 require "data/elections_2016"
 
-list = D3.select("#visualization").append("ul")
+list = D3.select("#visualization").append("ul").attr("class", "results")
 max_votes = Elections2016.map(&:votes).max
 
 Elections2016.each do |candidate|
   vote_count_formatted = D3.format("3,d").(candidate.votes)
   item = list.append("li")
-    .style("position", "relative")
   item.append("span")
+    .attr("class", "vote-bar")
     .style("background-color", candidate.color)
-    .style("opacity", 0.5)
-    .style("position", "absolute")
     .style("width", "#{100.0 * candidate.votes / max_votes}%")
-    .style("height", "100%")
   item.append("span")
-    .style("position", "absolute")
+    .attr("class", "vote-description")
     .text("#{candidate.name} - #{vote_count_formatted} votes")
 end
