041327ca8759aefb74404d6c002fbfeddfa4c578
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-02-18 00:17:07 +0000

    Improved visualization: Weather in London

--- demo/app/weather_in_london.rb
+++ demo/app/weather_in_london.rb
@@ -1,4 +1,3 @@
-require "opal"
 require "opal-d3"
 require "data/weather_in_london"
 
@@ -25,37 +24,37 @@ y = D3.scale_linear.domain([min_temp, max_temp]).range([180, 20])
     .attr("height", 180)
     .attr("fill", "lightgrey")
 
-  data = WeatherInLondon.select{|row| row.month == month}
+  data = WeatherInLondon.select{|row_data| row_data.month == month}
 
   max_temp = D3.line
-                .x{|d| x.(d.day) }
-                .y{|d| y.(d.max) }
-                .curve(D3.curve_natural)
+    .x{|d| x.(d.day) }
+    .y{|d| y.(d.max) }
+    .curve(D3.curve_natural)
   svg.append("path")
-      .attr("d", max_temp.(data))
-      .attr("stroke", "red")
-      .attr("stroke-width", "2px")
-      .attr("fill", "none")
+    .attr("d", max_temp.(data))
+    .attr("stroke", "red")
+    .attr("stroke-width", "2px")
+    .attr("fill", "none")
 
   mean_temp = D3.line
-                .x{|d| x.(d.day) }
-                .y{|d| y.(d.mean) }
-                .curve(D3.curve_natural)
+    .x{|d| x.(d.day) }
+    .y{|d| y.(d.mean) }
+    .curve(D3.curve_natural)
   svg.append("path")
-      .attr("d", mean_temp.(data))
-      .attr("stroke", "black")
-      .attr("stroke-width", "2px")
-      .attr("fill", "none")
+    .attr("d", mean_temp.(data))
+    .attr("stroke", "black")
+    .attr("stroke-width", "2px")
+    .attr("fill", "none")
 
   min_temp = D3.line
-                .x{|d| x.(d.day) }
-                .y{|d| y.(d.min) }
-                .curve(D3.curve_natural)
+    .x{|d| x.(d.day) }
+    .y{|d| y.(d.min) }
+    .curve(D3.curve_natural)
   svg.append("path")
-      .attr("d", min_temp.(data))
-      .attr("stroke", "blue")
-      .attr("stroke-width", "2px")
-      .attr("fill", "none")
+    .attr("d", min_temp.(data))
+    .attr("stroke", "blue")
+    .attr("stroke-width", "2px")
+    .attr("fill", "none")
 
   axis_left = D3.axis_left(y).tick_size(2)
   svg.append("g").attr("transform", "translate(20,0)").call(axis_left)
